285a4d045f72d1a7db70d25408d8860dd338364f
Aniruddha <anigayake101@gmail.com>
2022-02-06 15:41:28 +0530

    Experience section added. animated exper. few other enhancements

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,21 @@ if(scrollToTopBtn){
   scrollToTopBtn.addEventListener("click",scrollToTop);
 }
 
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
 // const menu = document.querySelector(".navigation");
 // const menuItems = document.querySelectorAll(".nav-link");
 // const hamburger= document.querySelector(".burger-menu");
